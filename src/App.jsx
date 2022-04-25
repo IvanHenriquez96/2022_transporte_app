8793df672db31bde2880c8f90bd0e3a096315fb3
IvanHenriquez96 <iv.henriquez.96@gmail.com>
2022-04-25 01:01:35 -0400

    funcionlidad basica hecha, mejorar

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import { FormularioParadero } from './components/FormularioParadero';
+
+
+function App() {
+  return (
+    <div className="App container">
+      
+      <h1 className="text-center mb-5">Transporte APP</h1>
+
+      <FormularioParadero />
+
+      
+
+
+    </div>
+  );
+}
+
+export default App;
